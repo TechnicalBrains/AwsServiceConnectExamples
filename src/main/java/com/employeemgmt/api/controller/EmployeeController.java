03a9366a537c2d576e8fbba455477ba632168732
Technological Framework Samples <ramakanth.confident@gmail.com>
2025-01-07 18:38:53 +0530

    Added slf4j

--- src/main/java/com/employeemgmt/api/controller/EmployeeController.java
+++ src/main/java/com/employeemgmt/api/controller/EmployeeController.java
@@ -4,6 +4,8 @@
 package com.employeemgmt.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +16,7 @@ import com.employeemgmt.api.service.EmployeeService;
  * 
  */
 @RestController
+@CrossOrigin
 public class EmployeeController {
 	
 	@Autowired
@@ -22,6 +25,11 @@ public class EmployeeController {
 	@PostMapping(value = "/create")
 	public void createEmployee(Employee employee) {
 		employeeService.createEmployee(employee);	
+	}
+	
+	@GetMapping(value = "/checkHealth")
+	public void checkHealth() {
+		employeeService.checkDeptServiceHealth();	
 	}
 	
 }
--- src/main/java/com/employeemgmt/api/service/EmployeeService.java
+++ src/main/java/com/employeemgmt/api/service/EmployeeService.java
@@ -2,6 +2,8 @@ package com.employeemgmt.api.service;
 
 import java.util.HashMap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -9,20 +11,31 @@ import org.springframework.web.client.RestTemplate;
 
 import com.employeemgmt.api.domain.Employee;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * 
  */
 @Service
+@Slf4j
 public class EmployeeService {
 
 	@Autowired
 	RestTemplate restTemplate;
 	
+	Logger logger = LoggerFactory.getLogger(EmployeeService.class);
+	
 	public String createEmployee(Employee employee) {
-		ResponseEntity<String> response = restTemplate.postForEntity("http://department-mgmt-service.dept-mgmt-ns:8080/api/department/create",
+		ResponseEntity<String> response = restTemplate.postForEntity("http://dept-mgm-svc-dns:8080/api/department/create",
 				new HashMap<>(),String.class);
 		System.out.println(" Response Status Code :  "+response.getStatusCode().value());
 		System.out.println(" Response Body:  "+response.getBody());
 		return response.getBody();
+	}
+	
+	public String checkDeptServiceHealth() {
+		String response = restTemplate.getForObject("http://dept-mgm-svc-dns:8080/api/department/actuator/health",String.class);
+		logger.info(" Response Status Code {}  ",response);
+		return response;
 	}
 }
